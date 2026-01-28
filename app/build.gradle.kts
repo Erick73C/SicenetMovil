a20dee632ddbb1109148e7d437f1de66b8f8c983
Erick Omar Perez Gonzlaez <s22120250@alumnos.itsur.edu.mx>
2026-01-27 22:09:59 -0600

    Se agregaron dependencias y permisos que la app necesita

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,13 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
